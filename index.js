cd887413a968de02600fa3e302329242825e9119
a17towan <92051242+Xijet@users.noreply.github.com>
2025-04-07 17:05:43 +0200

    Adding multiple pages
    Instead of doing a single page application, I am making multiple .html files for my pages because the size of the scope got too big.

--- index.js
+++ index.js
@@ -1,41 +1,25 @@
-const pages = document.getElementsByClassName("page");
 
-function hideGallery() {
-  document.getElementById("imgGallery").style.display = "none";
-  console.log(imgGallery.value);
-}
-
-function home() {
-  imgGallery.style.display = "grid";
-}
-
-function hideOnload() {
-  let pagesToHide = document.getElementsByClassName("page"); // Putting the pages in an array
-  for (let i = 0; i < pagesToHide.length; i++) {
-    pagesToHide[i].style.display = "none"; // Hiding the pages that shouldn't be visible
-  }
-}
 
 function displayImages() {
   let gallery = document.getElementById("imgGallery");
   let images = [];
 
-  // Find a way to handle if there is no path to file so I don't get a bunch of path errors
-  
   for (let i = 0; i <= 8; i++) {
-      images.push(`img/cozmo${i}` + ".jpg");
+    images.push(`img/cozmo${i}` + ".jpg");
   }
 
   images.forEach((imageUrl) => {
     let img = document.createElement("img");
     let list = document.createElement("div");
-    list.className = "imgDiv";
+    list.className = "imgDiv"; // Creating containers for the images and giving them a class
 
     img.src = imageUrl;
 
     list.appendChild(img);
-    gallery.appendChild(list);
+    gallery.appendChild(list); // Inserting the new images in the existing div in the DOM
   });
+
+  // Find a way to handle if there is no path to file so I don't get a bunch of path errors
 }
 
 displayImages();
